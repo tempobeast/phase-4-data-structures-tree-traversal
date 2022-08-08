6e0040ed0495bbc907d0421e7ba59929f0a6ec16
Zach <shaw.zacht@gmail.com>
2022-08-08 13:48:38 -0700

    commit tree 7d9be9311476a680e103174b8ad11dc4de09f44c

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,23 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id(string)
+    result = []
+    nodes_to_visit = [@root]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      if node[:id] == string
+        return node
+      else
+      result.push(node[:id])
+      nodes_to_visit = node[:children] + nodes_to_visit
+      end
+      nil
+    end
+
+  end
+
+
 end
